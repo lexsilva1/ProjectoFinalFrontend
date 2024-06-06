72f7cf181d5b98cf20cda213531e05d7d77778a8
Ines Cunha <inescunha@student.dei.uc.pt>
2024-06-06 01:11:22 +0100

    pagina de perfil2

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,6 +7,8 @@ import { findUserById, uploadUserPhoto } from "../services/userServices";
 import Cookies from "js-cookie";
 import userStore from "../stores/userStore";
 import { PencilSquare } from "react-bootstrap-icons";
+import "./Profile.css";
+import { FiLock, FiUnlock } from "react-icons/fi";
 
 const Profile = () => {
   const [profile, setProfile] = useState(null);
@@ -53,6 +55,7 @@ const Profile = () => {
       findUserById(token, user.id)
         .then((userFromServer) => {
           console.log(userFromServer);
+          setProfile(userFromServer);
         })
         .catch((error) => console.error(error));
     }
@@ -60,43 +63,33 @@ const Profile = () => {
 
   return (
     <>
-      <Header style={{ top: 0, width: "100%", height: "60px", zIndex: 1000 }} />
-      <div style={{ display: "flex" }}>
-        <div style={{ flex: "0 0 200px" }}>
+      <Header className="profile-header" />
+      <div className="profile-flex-container">
+        <div className="profile-sidebar">
           <Sidebar />
         </div>
-        <div style={{ flex: "1" }}>
+        <div className="profile-main-content">
           <Container fluid>
             <Row>
               <Col md={12}>
-                <Card
-                  style={{ width: "80%", height: "120%", position: "relative" }}
-                >
+                <Card className="profile-card">
                   <Card.Img
+                    className="profile-card-img"
                     src={user?.image ? user.image : Avatar}
-                    style={{
-                      width: "200px",
-                      height: "200px",
-                      borderRadius: "50%",
-                      position: "absolute",
-                      top: "20px",
-                      left: "20%",
-                      transform: "translateX(-50%)",
-                    }}
                   />
-                  <Card.Header
-                    style={{
-                      backgroundColor: "var(--bs-body-color)",
-                      color: "white",
-                      paddingTop: "120px",
-                      textAlign: "center",
-                    }}
-                  >
-                    <div style={{ position: 'absolute', top: '220px', left: '20%', transform: 'translateX(-50%)', display: 'flex', alignItems: 'center' }}>
-                      <Card.Title className="user-name2">
-                        {`${user?.firstName} ${user?.lastName}`}
-                      </Card.Title>
-                      <Button variant="outline-secondary" onClick={() => setEditMode(!editMode)} style={{ marginLeft: '10px' }}>
+                  <Card.Header className="profile-card-header">
+                    <Card.Img
+                      className="profile-card-img"
+                      src={user?.image ? user.image : Avatar}
+                    />
+                    <div className="profile-nameedit">
+                      {`${user?.firstName} ${user?.lastName}`}
+
+                      <Button
+                        variant="outline-secondary"
+                        onClick={() => setEditMode(!editMode)}
+                        className="profile-edit-button"
+                      >
                         <PencilSquare />
                       </Button>
                     </div>
@@ -106,15 +99,12 @@ const Profile = () => {
                       {editMode && (
                         <>
                           <input type="file" onChange={handleImageUpload} />
-                          <Button variant="outline-secondary" className="mt-2">
-                            Change profile photo
-                          </Button>
                         </>
                       )}
                     </Col>
                     <Col md={8}>
                       <Card.Body>
-                        <div style={{ marginTop: "20px" }}>
+                        <div>
                           {editMode ? (
                             <Form>
                               <Form.Group>
@@ -122,7 +112,7 @@ const Profile = () => {
                                 <Form.Control
                                   type="text"
                                   placeholder="First Name"
-                                  defaultValue={user?.firstName}
+                                  defaultValue={profile?.firstName}
                                 />
                               </Form.Group>
                               <Form.Group>
@@ -130,7 +120,7 @@ const Profile = () => {
                                 <Form.Control
                                   type="text"
                                   placeholder="Last Name"
-                                  defaultValue={user?.lastName}
+                                  defaultValue={profile?.lastName}
                                 />
                               </Form.Group>
                               <Form.Group>
@@ -138,7 +128,23 @@ const Profile = () => {
                                 <Form.Control
                                   type="text"
                                   placeholder="Nickname"
-                                  defaultValue={user?.nickname}
+                                  defaultValue={profile?.nickname}
+                                />
+                              </Form.Group>
+                              <Form.Group>
+                                <Form.Label>Usual Work Place:</Form.Label>
+                                <Form.Control
+                                  type="text"
+                                  placeholder="Usual Work Place"
+                                  defaultValue={profile?.labLocation}
+                                />
+                              </Form.Group>
+                              <Form.Group>
+                                <Form.Label>Bio:</Form.Label>
+                                <Form.Control
+                                  type="text"
+                                  placeholder="Bio"
+                                  defaultValue={profile?.bio}
                                 />
                               </Form.Group>
                               <Button
@@ -151,17 +157,47 @@ const Profile = () => {
                             </Form>
                           ) : (
                             <>
-                              <Card.Text>
+                              <Card.Text className="profile-info-text">
                                 <strong>First Name:</strong>{" "}
-                                {user?.firstName || ""}
+                                {profile?.firstName || ""}
                               </Card.Text>
-                              <Card.Text>
+                              <Card.Text className="profile-info-text">
                                 <strong>Last Name:</strong>{" "}
-                                {user?.lastName || ""}
+                                {profile?.lastName || ""}
                               </Card.Text>
-                              <Card.Text>
+                              <Card.Text className="profile-info-text">
                                 <strong>Nickname:</strong>{" "}
-                                {user?.nickname || ""}
+                                {profile?.nickname || ""}
+                              </Card.Text>
+                              <Card.Text className="profile-info-text">
+                                <strong>Usual Work Place:</strong>{" "}
+                                {profile?.labLocation || ""}
+                              </Card.Text>
+                              <Card.Text className="profile-info-text">
+                                <strong>Bio:</strong> {profile?.bio || ""}
+                              </Card.Text>
+                              <Card.Text className="profile-info-text">
+                                <strong>Skills:</strong>{" "}
+                                {profile?.skills.length > 0
+                                  ? profile.skills.join(", ")
+                                  : "No skills added"}
+                              </Card.Text>
+                              <Card.Text className="profile-info-text">
+                                <strong>Interests:</strong>{" "}
+                                {profile?.interests.length > 0
+                                  ? profile.interests.join(", ")
+                                  : "No interests added"}
+                              </Card.Text>
+                              <Card.Text className="profile-info-text">
+                                <strong>Projects:</strong>{" "}
+                                {profile?.projects.length > 0
+                                  ? profile.projects.map((project, index) => (
+                                      <div key={index}>
+                                        <strong>{project.name}</strong>
+                                        <p>{project.description}</p>
+                                      </div>
+                                    ))
+                                  : "No projects added"}
                               </Card.Text>
                             </>
                           )}
@@ -177,6 +213,6 @@ const Profile = () => {
       </div>
     </>
   );
-}
-  
-  export default Profile;
+};
+
+export default Profile;
