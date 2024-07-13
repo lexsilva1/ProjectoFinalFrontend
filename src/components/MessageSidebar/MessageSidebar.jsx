d4166ebefbe345ff5b8345d68d199abb46f9b2b9
lexsilva1 <jagmsilva@student.dei.uc.pt>
2024-07-13 06:31:27 +0100

    mensagens

--- src/components/MessageSidebar/MessageSidebar.jsx
+++ src/components/MessageSidebar/MessageSidebar.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { FaInbox, FaEnvelope } from 'react-icons/fa';
 import './MessageSidebar.css';
+import { getLastMessages } from '../../services/messageServices';
 
 
 
-const MessageSidebar = ({ onSearch, onInboxClick }) => {
+const MessageSidebar = ({ onSearch, onInboxClick, showUnread,search,setSearch }) => {
 
   
   return (
     <div className="message-sidebar">
       <h5>Messages</h5>
-      <input className="search-users" type="text" placeholder="Search..." onChange={onSearch} />
+      <input className="search-users" type="text" placeholder="Search..." onChange={onSearch} value={search} />
       <div className="message-icons">
       <div className="inbox-icon" onClick={onInboxClick}><FaInbox /> Inbox</div>
-        <div className="unread-icon"><FaEnvelope /> Unread</div>
+        <div className="unread-icon" onClick={showUnread}><FaEnvelope /> Unread</div>
       </div>
     </div>
   );
--- src/pages/Messages.jsx
+++ src/pages/Messages.jsx
@@ -16,24 +16,55 @@ import MessageSidebar from "../components/MessageSidebar/MessageSidebar";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Messages.css";
+import { set } from "date-fns";
 
 
 const Messages = () => {
   const userList = userStore((state) => state.userList);
+  const setUserList = userStore((state) => state.setUserList);
   const setSelectedMessages = userStore((state) => state.setSelectedMessages);
   const selectedUserMessages = userStore((state) => state.selectedUserMessages);
   const token = Cookies.get("authToken");
   const navigate = useNavigate();
-  const setUnreadMessages = userStore((state) => state.setUnreadMessages);
+  
+  const [search, setSearch] = useState("");
+  const [unreadMessages, setUnreadMessages] = useState(true);
 
-  const resetSelectedUserMessages = () => {
+  const resetSelectedUserMessages = async () => {
     navigate("/messages");
     userStore.setState({ selectedUserMessages: null });
+    setSelectedMessages(null);
+    setUnreadMessages(false);
+const response = await getLastMessages(token);
+    setUserList(response);
   };
   console.log('selectedMessages',selectedUserMessages);
   console.log('userList',userList);
+const handleUnreadMessages = async () => {
+  setUnreadMessages(true);
+  const unread = userList.filter((user) => user.read === false);
+  setUserList(unread);
+  setSearch('');
+};
 
 
+const handleSearch = async (e) => {
+  const value = e.target.value;
+  setSearch(value); // This sets the state for future renders, not for immediate use below
+
+  if (value.trim() === '') {
+    // If search input is empty, reset the userList to its original state
+    const response = await getLastMessages(token);
+    setUserList(response);
+  } else {
+    // If search is not empty, filter the userList
+    const filteredUsers = userList.filter((user) =>
+      user.sender.firstName.toLowerCase().includes(value.toLowerCase()) || 
+      user.sender.lastName.toLowerCase().includes(value.toLowerCase())
+    );
+    setUserList(filteredUsers);
+  }
+};
 
 
 
@@ -50,7 +81,12 @@ const Messages = () => {
           <div>
             <Row>
               <Col md={2} className="side-bar-column">
-                <MessageSidebar onInboxClick={resetSelectedUserMessages} />
+                <MessageSidebar onInboxClick={() => {resetSelectedUserMessages();setSearch('')}}
+                 onSearch={handleSearch} 
+                 showUnread={handleUnreadMessages} 
+                 unreadMessages={unreadMessages} 
+                 setUnreadMessages={setUnreadMessages} 
+                 search={search} setSearch={setSearch}/>
               </Col>
               <Col md={10}>
                 {selectedUserMessages === null ? (
